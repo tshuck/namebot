f3af4d826b9a56e5654186a879a57b51297cd3b9
Truman <truman.shuck@gmail.com>
2017-01-04 00:09:04 -0600

    Remove token

--- app/app.js
+++ app/app.js
@@ -5,11 +5,9 @@ import Messenger from './messenger.js';
 
 import Botkit from 'botkit';
 
-const slack_token = 'xoxb-123124703397-zGfOtUn0nM16sQ8z3TOzmwBo';
-
 const controller = Botkit.slackbot();
 const bot = controller.spawn({
-  token: slack_token
+  token: process.env.SLACK_TOKEN
 });
 
 const generator = new Generator();
@@ -18,10 +16,8 @@ generator
   .loadCacheFromCSV()
   .then(function() {
     bot.startRTM();
-    
-    console.log(generator.has);
-    const messenger = new Messenger(generator);
 
+    const messenger = new Messenger(generator);
     controller.on('ambient', (bot, message) => messenger.ambient(bot, message));
     controller.on('direct_mention', (bot, message) => messenger.mention(bot, message));
   });
